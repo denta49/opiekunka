fb91844aae108b6fbc77766992a2d86ab945fc8e
Przemysław Wojenka <p.wojenka@361.sh>
2025-08-26 16:55:55 +0200

    earn command implementation and stored in memory db

--- loyalty/__main__.py
+++ loyalty/__main__.py
@@ -1,6 +1,8 @@
 import argparse
 import sys
 
+balances: dict[str, int] = {}
+
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -20,13 +22,28 @@ def build_parser() -> argparse.ArgumentParser:
     return parser
 
 
+def earn(customer_id: str, points: int) -> int:
+    if points < 0:
+        raise ValueError("Points must be a positive int.")
+    new_balance = balances.get(customer_id, 0) + points
+    balances[customer_id] = new_balance
+    return balances[customer_id]
+
+
 def main() -> int:
     print('CLI is live, You can start!')
 
     parser = build_parser()
     args = parser.parse_args()
 
-    print("parsed:", args)
+    if args.command == "earn":
+        try:
+            new_balance = earn(args.customer_id, args.points)
+            print(f"Customer {args.customer_id} balance is now {new_balance} points.")
+            return 0
+        except ValueError as e:
+            print(f"Error: {e}")
+            return 1
     return 0
 
 

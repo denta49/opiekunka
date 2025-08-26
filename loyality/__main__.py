da66893f81b429044dcff2e1a09c43cbbc195f28
Przemysław Wojenka <p.wojenka@361.sh>
2025-08-26 16:43:51 +0200

    add parser and commands

--- loyality/__main__.py
+++ loyality/__main__.py
@@ -1,9 +1,32 @@
+import argparse
 import sys
 
 
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog="loyalty",
+        description="Customer Loyalty Points CLI"
+    )
+    sub = parser.add_subparsers(dest="command", required=True)
+
+    p_earn = sub.add_parser("earn", help="Add points to customer balance")
+    p_earn.add_argument("customer_id", type=str)
+    p_earn.add_argument("points", type=int)
+
+    p_redeem = sub.add_parser("redeem", help="Redeem points from a customer's balance")
+    p_redeem.add_argument("customer_id", type=str)
+    p_redeem.add_argument("points", type=int)
+
+    return parser
+
+
 def main() -> int:
-    print('CLI is live')
-    print(sys.argv[1:])
+    print('CLI is live, You can start!')
+
+    parser = build_parser()
+    args = parser.parse_args()
+
+    print("parsed:", args)
     return 0
 
 
